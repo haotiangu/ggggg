89579e9d974ef20e359905198d19e5ee987a51d2
Haotian Gu <47264363+haotiangu@users.noreply.github.com>
2023-05-03 20:44:22 -0400

    Add files via upload

---
+++ scripts/node_SWINIR.py
@@ -0,0 +1,372 @@
+#!/usr/bin/env python3
+import numpy as np
+import rospy, cv2
+import torch
+import os, sys
+
+import argparse
+import time
+import torch.nn as nn
+
+import signal
+
+from std_msgs.msg import Float32MultiArray        # See https://gist.github.com/jarvisschultz/7a886ed2714fac9f5226
+from std_msgs.msg import Float32
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+from geometry_msgs.msg import Twist
+
+from agents.image_attack_agent import ImageAttacker
+from setting_params import FREQ_MID_LEVEL, SETTING
+
+import glob
+
+from collections import OrderedDict
+import requests
+
+from fastdvdnet.network_swinir import SwinIR as net
+#from utils import util_calculate_psnr_ssim as util
+
+
+
+IMAGE_CLEAN_RECEIVED = None
+def fnc_clean_img_callback(msg):
+    global IMAGE_CLEAN_RECEIVED
+    IMAGE_CLEAN_RECEIVED = msg
+
+IMAGE_RECEIVED = None
+def fnc_img_callback(msg):
+    global IMAGE_RECEIVED
+    IMAGE_RECEIVED = msg
+
+ADV_IMAGE_RECEIVED = None
+def fnc_adv_img_callback(msg):
+    global ADV_IMAGE_RECEIVED
+    ADV_IMAGE_RECEIVED = msg
+
+def define_model(args):
+    # 001 classical image sr
+    if args.task == 'classical_sr':
+        model = net(upscale=args.scale, in_chans=3, img_size=args.training_patch_size, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='pixelshuffle', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 002 lightweight image sr
+    # use 'pixelshuffledirect' to save parameters
+    elif args.task == 'lightweight_sr':
+        model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6], embed_dim=60, num_heads=[6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='pixelshuffledirect', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 003 real-world image sr
+    elif args.task == 'real_sr':
+        if not args.large_model:
+            # use 'nearest+conv' to avoid block artifacts
+            model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
+                        img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                        mlp_ratio=2, upsampler='nearest+conv', resi_connection='1conv')
+        else:
+            # larger model size; use '3conv' to save parameters and memory; use ema for GAN training
+            model = net(upscale=args.scale, in_chans=3, img_size=64, window_size=8,
+                        img_range=1., depths=[6, 6, 6, 6, 6, 6, 6, 6, 6], embed_dim=240,
+                        num_heads=[8, 8, 8, 8, 8, 8, 8, 8, 8],
+                        mlp_ratio=2, upsampler='nearest+conv', resi_connection='3conv')
+        param_key_g = 'params_ema'
+
+    # 004 grayscale image denoising
+    elif args.task == 'gray_dn':
+        model = net(upscale=1, in_chans=1, img_size=128, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 005 color image denoising
+    elif args.task == 'color_dn':
+        model = net(upscale=1, in_chans=3, img_size=128, window_size=8,
+                    img_range=1., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 006 grayscale JPEG compression artifact reduction
+    # use window_size=7 because JPEG encoding uses 8x8; use img_range=255 because it's sligtly better than 1
+    elif args.task == 'jpeg_car':
+        model = net(upscale=1, in_chans=1, img_size=126, window_size=7,
+                    img_range=255., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    # 006 color JPEG compression artifact reduction
+    # use window_size=7 because JPEG encoding uses 8x8; use img_range=255 because it's sligtly better than 1
+    elif args.task == 'color_jpeg_car':
+        model = net(upscale=1, in_chans=3, img_size=126, window_size=7,
+                    img_range=255., depths=[6, 6, 6, 6, 6, 6], embed_dim=180, num_heads=[6, 6, 6, 6, 6, 6],
+                    mlp_ratio=2, upsampler='', resi_connection='1conv')
+        param_key_g = 'params'
+
+    pretrained_model = torch.load(args.model_path)
+    model.load_state_dict(pretrained_model[param_key_g] if param_key_g in pretrained_model.keys() else pretrained_model, strict=True)
+
+    return model
+
+
+def setup(args):
+    # 001 classical image sr/ 002 lightweight image sr
+    if args.task in ['classical_sr', 'lightweight_sr']:
+        save_dir = f'results/swinir_{args.task}_x{args.scale}'
+        folder = args.folder_gt
+        border = args.scale
+        window_size = 8
+
+    # 003 real-world image sr
+    elif args.task in ['real_sr']:
+        save_dir = f'results/swinir_{args.task}_x{args.scale}'
+        if args.large_model:
+            save_dir += '_large'
+        folder = args.folder_lq
+        border = 0
+        window_size = 8
+
+    # 004 grayscale image denoising/ 005 color image denoising
+    elif args.task in ['gray_dn', 'color_dn']:
+        save_dir = f'results/swinir_{args.task}_noise{args.noise}'
+        folder = args.folder_gt
+        border = 0
+        window_size = 8
+
+    # 006 JPEG compression artifact reduction
+    elif args.task in ['jpeg_car', 'color_jpeg_car']:
+        save_dir = f'results/swinir_{args.task}_jpeg{args.jpeg}'
+        folder = args.folder_gt
+        border = 0
+        window_size = 7
+
+    return folder, save_dir, border, window_size
+
+def get_image_pair(args, path):
+ 
+    # 001 classical image sr/ 002 lightweight image sr (load lq-gt image pairs)
+    if args.task in ['classical_sr', 'lightweight_sr']:
+        img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
+        img_lq = cv2.imread(f'{args.folder_lq}/{imgname}x{args.scale}{imgext}', cv2.IMREAD_COLOR).astype(
+            np.float32) / 255.
+
+    # 003 real-world image sr (load lq image only)
+    elif args.task in ['real_sr']:
+        img_gt = None
+        #img_lq = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
+        img_lq = (cv2.cvtColor(path, cv2.IMREAD_COLOR)).astype(np.float32) / 255.
+
+    # 004 grayscale image denoising (load gt image and generate lq image on-the-fly)
+    elif args.task in ['gray_dn']:
+        img_gt = cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype(np.float32) / 255.
+        np.random.seed(seed=0)
+        img_lq = img_gt + np.random.normal(0, args.noise / 255., img_gt.shape)
+        img_gt = np.expand_dims(img_gt, axis=2)
+        img_lq = np.expand_dims(img_lq, axis=2)
+
+    # 005 color image denoising (load gt image and generate lq image on-the-fly)
+    elif args.task in ['color_dn']:
+        img_gt = cv2.imread(path, cv2.IMREAD_COLOR).astype(np.float32) / 255.
+        np.random.seed(seed=0)
+        img_lq = img_gt + np.random.normal(0, args.noise / 255., img_gt.shape)
+
+    # 006 grayscale JPEG compression artifact reduction (load gt image and generate lq image on-the-fly)
+    elif args.task in ['jpeg_car']:
+        img_gt = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        if img_gt.ndim != 2:
+            img_gt = util.bgr2ycbcr(img_gt, y_only=True)
+        result, encimg = cv2.imencode('.jpg', img_gt, [int(cv2.IMWRITE_JPEG_QUALITY), args.jpeg])
+        img_lq = cv2.imdecode(encimg, 0)
+        img_gt = np.expand_dims(img_gt, axis=2).astype(np.float32) / 255.
+        img_lq = np.expand_dims(img_lq, axis=2).astype(np.float32) / 255.
+
+    # 006 JPEG compression artifact reduction (load gt image and generate lq image on-the-fly)
+    elif args.task in ['color_jpeg_car']:
+        img_gt = cv2.imread(path)
+        result, encimg = cv2.imencode('.jpg', img_gt, [int(cv2.IMWRITE_JPEG_QUALITY), args.jpeg])
+        img_lq = cv2.imdecode(encimg, 1)
+        img_gt = img_gt.astype(np.float32)/ 255.
+        img_lq = img_lq.astype(np.float32)/ 255.
+
+    return img_lq, img_gt
+
+
+def test(img_lq, model, args, window_size):
+    if args.tile is None:
+        # test the image as a whole
+        output = model(img_lq)
+    else:
+        # test the image tile by tile
+        b, c, h, w = img_lq.size()
+        tile = min(args.tile, h, w)
+        assert tile % window_size == 0, "tile size should be a multiple of window_size"
+        tile_overlap = args.tile_overlap
+        sf = args.scale
+
+        stride = tile - tile_overlap
+        h_idx_list = list(range(0, h-tile, stride)) + [h-tile]
+        w_idx_list = list(range(0, w-tile, stride)) + [w-tile]
+        E = torch.zeros(b, c, h*sf, w*sf).type_as(img_lq)
+        W = torch.zeros_like(E)
+
+        for h_idx in h_idx_list:
+            for w_idx in w_idx_list:
+                in_patch = img_lq[..., h_idx:h_idx+tile, w_idx:w_idx+tile]
+                out_patch = model(in_patch)
+                out_patch_mask = torch.ones_like(out_patch)
+
+                E[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch)
+                W[..., h_idx*sf:(h_idx+tile)*sf, w_idx*sf:(w_idx+tile)*sf].add_(out_patch_mask)
+        output = E.div_(W)
+
+    return output
+
+
+def get_args():
+    """ Get arguments for individual tb3 deployment. """
+    parser = argparse.ArgumentParser(
+        description="Denoise a sequence with SWINIR"
+    )
+    parser.add_argument('--task', type=str, default='real_sr', help='classical_sr, lightweight_sr, real_sr, '
+                                                                     'gray_dn, color_dn, jpeg_car, color_jpeg_car')
+    parser.add_argument('--scale', type=int, default=4, help='scale factor: 1, 2, 3, 4, 8') # 1 for dn and jpeg car
+    parser.add_argument('--noise', type=int, default=15, help='noise level: 15, 25, 50')
+    parser.add_argument('--jpeg', type=int, default=40, help='scale factor: 10, 20, 30, 40')
+    parser.add_argument('--training_patch_size', type=int, default=128, help='patch size used in training SwinIR. '
+                                       'Just used to differentiate two different settings in Table 2 of the paper. '
+                                       'Images are NOT tested patch by patch.')
+    parser.add_argument('--large_model', action='store_true', help='use large model, only provided for real image sr')
+    parser.add_argument('--model_path', type=str,
+                        default='/home/haotiangu/catkin_ws/src/tcps_image_attack/scripts/fastdvdnet/experiments/pretrained_models/003_realSR_BSRGAN_DFO_s64w8_SwinIR-M_x4_GAN.pth')
+    parser.add_argument('--folder_lq', type=str, default=None, help='input low-quality test image folder')
+    parser.add_argument('--folder_gt', type=str, default=None, help='input ground-truth test image folder')
+    parser.add_argument('--tile', type=int, default=800, help='Tile size, None for no tile during testing (testing as a whole)')
+    parser.add_argument('--tile_overlap', type=int, default=32, help='Overlapping of different tiles')
+
+    return parser.parse_known_args(sys.argv)
+
+
+if __name__ == '__main__':
+
+    # rosnode node initialization
+    rospy.init_node('SWINIR_node')
+    print('SWINIR_node is initialized at', os.getcwd())
+    start_time = time.time()
+    args, unknown = get_args()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    # set up model
+    if os.path.exists(args.model_path):
+        print(f'loading model from {args.model_path}')
+    else:
+        os.makedirs(os.path.dirname(args.model_path), exist_ok=True)
+        url = 'https://github.com/JingyunLiang/SwinIR/releases/download/v0.0/{}'.format(os.path.basename(args.model_path))
+        r = requests.get(url, allow_redirects=True)
+        print(f'downloading model {args.model_path}')
+        open(args.model_path, 'wb').write(r.content)
+  
+    for p, v in zip(args.__dict__.keys(), args.__dict__.values()):
+		      print('{}: {}'.format(p, v))
+
+    model = define_model(args)
+    model.eval()
+    model = model.to(device)
+
+
+    # setup folder and path
+    folder, save_dir, border, window_size = setup(args)
+    os.makedirs(save_dir, exist_ok=True)
+    test_results = OrderedDict()
+    test_results['psnr'] = []
+    test_results['ssim'] = []
+    test_results['psnr_y'] = []
+    test_results['ssim_y'] = []
+    test_results['psnrb'] = []
+    test_results['psnrb_y'] = []
+    psnr, ssim, psnr_y, ssim_y, psnrb, psnrb_y = 0, 0, 0, 0, 0, 0
+
+    mid_time = time.time()
+    # subscriber init.
+    sub_clean_image = rospy.Subscriber('/airsim_node/camera_frame', Image, fnc_clean_img_callback)
+    #sub_attacked_image  = rospy.Subscriber('/attack_generator_node/attacked_image', Image, fnc_img_callback)
+    #sub_adv_image = rospy.Subscriber('/attack_generator_node/perturbation_image', Image, fnc_adv_img_callback)
+    # publishers init.
+    pub_clean_image = rospy.Publisher('/fastdvdnet_node/clean_image', Image, queue_size=10)
+
+    # Running rate
+    rate=rospy.Rate(FREQ_MID_LEVEL)
+    # a bridge from cv2 image to ROS image
+    mybridge = CvBridge()
+
+    error_count = 0
+    n_iteration = 0
+
+    seq_list = []
+
+    ##############################
+    ### Instructions in a loop ###
+    ##############################
+
+    while not rospy.is_shutdown():
+
+        n_iteration += 1
+                
+        # Image generation
+        #if IMAGE_RECEIVED is not None and ADV_IMAGE_RECEIVED is not None and IMAGE_CLEAN_RECEIVED is not None:
+        # TRY THE REAL_TIME PERFORMANCE
+        if IMAGE_CLEAN_RECEIVED is not None:
+            with torch.no_grad():
+                # Get camera image
+                #np_clean_im = np.frombuffer(IMAGE_CLEAN_RECEIVED.data, dtype=np.uint8).reshape(IMAGE_CLEAN_RECEIVED.height, IMAGE_CLEAN_RECEIVED.width, -1)
+                #np_clean_im = np.array(np_clean_im)
+                #print('np_clean_im',np_clean_im.shape) #448*448*3
+                # Get attacked image
+                np_im = np.frombuffer(IMAGE_RECEIVED.data, dtype=np.uint8).reshape(IMAGE_RECEIVED.height, IMAGE_RECEIVED.width, -1)
+                np_im = np.array(np_im)
+                #print('np_im',np_im.shape)
+
+                # Get visualized noise image
+                #np_adv_im = np.frombuffer(ADV_IMAGE_RECEIVED.data, dtype=np.uint8).reshape(ADV_IMAGE_RECEIVED.height, ADV_IMAGE_RECEIVED.width, -1)
+                #np_adv_im = np.array(np_adv_im)
+                #print('np_adv_im',np_adv_im.shape)#(448,448,3)
+
+                # read attacked image 
+                path = np_im
+                img_lq, img_gt = get_image_pair(args, path)  # image to HWC-BGR, float32
+                img_lq = np.transpose(img_lq if img_lq.shape[2] == 1 else img_lq[:, :, [2, 1, 0]], (2, 0, 1))  # HCW-BGR to CHW-RGB
+                img_lq = torch.from_numpy(img_lq).float().unsqueeze(0).to(device)  # CHW-RGB to NCHW-RGB
+
+                    # inference
+                with torch.no_grad():
+                    # pad input image to be a multiple of window_size
+                    _, _, h_old, w_old = img_lq.size()
+                    h_pad = (h_old // window_size + 1) * window_size - h_old
+                    w_pad = (w_old // window_size + 1) * window_size - w_old
+                    img_lq = torch.cat([img_lq, torch.flip(img_lq, [2])], 2)[:, :, :h_old + h_pad, :]
+                    img_lq = torch.cat([img_lq, torch.flip(img_lq, [3])], 3)[:, :, :, :w_old + w_pad]
+                    seq_time = time.time()
+                    output = test(img_lq, model, args, window_size)
+                    output = output[..., :h_old * args.scale, :w_old * args.scale]
+                    res = output.data.cpu().numpy()[0]
+                    res = res.transpose(1, 2, 0)
+                    res = (res*255.).clip(0, 255).astype(np.uint8)
+                    res = cv2.cvtColor(res, cv2.COLOR_RGB2BGR)
+                    stop_time = time.time()
+		                                
+                adv_frame = mybridge.cv2_to_imgmsg(res)
+                pub_clean_image.publish(adv_frame)
+                mid_start_time = time.time()
+            
+            runtime = (stop_time - seq_time) 
+            print('runtime',runtime) # SWINIR 8.7S # SWINIR_LARGE 7.8S wrong yolo detection
+
+        try:
+            experiment_done_done = rospy.get_param('experiment_done')
+        except:
+            experiment_done_done = False
+        if experiment_done_done and n_iteration > FREQ_MID_LEVEL*3:
+            rospy.signal_shutdown('Finished 100 Episodes!')
+
+        rate.sleep()
